69aed2a72652292753ed1ac75d320e7f2e73ade4
Akashgks <gksakash7@gmail.com>
2026-04-02 06:35:17 +0530

    streamlit deploy

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+
+train = pd.read_csv("data/Training.csv")
+test = pd.read_csv("data/Testing.csv")
+
+train = train.drop(columns=['Unnamed: 133'], errors='ignore')
+
+X_train = train.drop("prognosis", axis=1)
+y_train = train["prognosis"]
+
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+symptoms = X_train.columns.tolist()
+display = [s.replace('_', ' ').title() for s in symptoms]
+
+st.title("Symptom-Diagnosis Predictor using multinomial Naive Bayes Model")
+st.write("Select your symptoms and click Predict to find out the disease.")
+
+st.subheader("Select Symptoms:")
+display = st.multiselect("Choose your symptoms", display)
+selected = [symptoms[display.index(s)] for s in display]
+
+if st.button("Predict"):
+    if len(selected) == 0:
+        st.warning("⚠️ Please select at least one symptom!")
+    else:
+        input = dict.fromkeys(symptoms, 0)
+        for s in selected:
+            input[s] = 1
+        input_df = pd.DataFrame([input])
+        pred1 = model.predict(input_df)
+        st.success(f"Predicted Disease: **{pred1[0]}**")
